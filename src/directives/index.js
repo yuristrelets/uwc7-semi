477a4ec09779251a90a547011a6a7cff952ad846
yuristrelets <yuristrelets@gmail.com>
2015-04-19 13:00:20 +0300

    add list item directive

--- src/directives/index.js
+++ src/directives/index.js
@@ -5,4 +5,6 @@ var app = angular.module('app.directives', []);
 require('./pagination/index').inject(app);
 require('./sort-pagination/index').inject(app);
 
+require('./list-item/index').inject(app);
+
 module.exports = app;
---
+++ src/directives/list-item/index.js
@@ -0,0 +1,24 @@
+exports.inject = function(app) {
+  app.directive('listItem', exports.factory);
+};
+
+exports.factory = function() {
+  return {
+    restrict: 'E',
+    replace: true,
+    scope: {
+      item: '='
+    },
+    template: require('./index.html'),
+    controller: _controller
+  }
+};
+
+function _controller($scope) {
+  angular.extend($scope, {
+
+  });
+
+  // ================================================
+
+}
